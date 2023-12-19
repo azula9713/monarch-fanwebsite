255b7dd3b6a1fdfe1d66f9c4ba21d8a8f537e3bb
Nilesh Viduranga <nnviduranga@gmail.com>
2023-12-19 14:52:09 +0530

    Refactor ShowGallery component to use constants

--- src/components/home/ShowGallery.tsx
+++ src/components/home/ShowGallery.tsx
@@ -3,50 +3,15 @@ import {
   ArrowRightCircleIcon,
 } from "@heroicons/react/20/solid";
 import { useState } from "react";
+
+import { rawShowData, showPosters } from "../../constants/Shows";
 import Title from "../common/Title";
 import ShowGalleryItem from "../custom/ShowGalleryItem";
 
 function ShowGallery() {
-  const [shows, setShows] = useState([
-    "https://4kwallpapers.com/images/walls/thumbs_3t/13192.jpg",
-    "https://r4.wallpaperflare.com/wallpaper/753/208/979/godzilla-godzilla-2014-wallpaper-ee6612133a8f97fcb4cfe4320c76fcfb.jpg",
-    "https://images7.alphacoders.com/113/1138319.jpg",
-    "https://images2.alphacoders.com/100/1001350.jpg",
-    "https://i.redd.it/3gxlgccrjim61.jpg",
-  ]);
+  const [shows, setShows] = useState(showPosters);
 
-  const [showData, setShowData] = useState([
-    {
-      id: 1,
-      title: "Monarch Legacy of Monsters",
-      description:
-        "Set after the battle between Godzilla and the Titans, revealing that monsters are real, follows one family's journey to uncover its buried secrets and a legacy linking them to Monarch.",
-    },
-    {
-      id: 2,
-      title: "Godzilla (2014)",
-      description:
-        "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity.",
-    },
-    {
-      id: 3,
-      title: "Kong: Skull Island",
-      description:
-        "After the Vietnam war, a team of scientists explores an uncharted island in the Pacific, venturing into the domain of the mighty Kong, and must fight to escape a primal Eden.",
-    },
-    {
-      id: 4,
-      title: "Godzilla: King of the Monsters",
-      description:
-        "The crypto-zoological agency Monarch faces off against a battery of god-sized monsters, including the mighty Godzilla, who collides with Mothra, Rodan, and his ultimate nemesis, the three-headed King Ghidorah.",
-    },
-    {
-      id: 5,
-      title: "Godzilla vs. Kong",
-      description:
-        "The epic next chapter in the cinematic Monsterverse pits two of the greatest icons in motion picture history against one another - the fearsome Godzilla and the mighty Kong - with humanity caught in the balance.",
-    },
-  ]);
+  const [showData, setShowData] = useState(rawShowData);
 
   const handleSlide = (direction: string) => {
     if (direction === "left") {
